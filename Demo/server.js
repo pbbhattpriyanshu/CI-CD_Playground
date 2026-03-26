5b47a921ffbd17b032394dfa5fa5991f702024bf
Priyanshu Bhatt <bhattpriyanshupcm007@gmail.com>
2026-03-26 21:36:06 +0530

    create new sum module to do sum of two numbers

--- Demo/server.js
+++ Demo/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import sum from './sum.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -6,6 +7,12 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+app.get('/getSum/:a/:b', async (req, res) => {
+    const { a, b } = req.params;
+    const result = sum(Number(a), Number(b));
+    res.send(`The sum of ${a} and ${b} is ${result}`);
+}); 
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
